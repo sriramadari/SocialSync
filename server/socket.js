e1603b84fff8b15532931055558433e5ebc1c581
SRIRAM ADARI <lakshmisriramadari1427@gmail.com>
2023-08-26 21:01:27 +0530

    deployment setup

--- server/socket.js
+++ server/socket.js
@@ -3,8 +3,7 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server,{
   cors:{
-    origin: ' http://localhost:5173',
-    methods: ['GET', 'POST'],
+    origin:"*"
   }
 })
 // const redisAdapter = require('socket.io-redis');
