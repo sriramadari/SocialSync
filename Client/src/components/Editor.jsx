d730257bf53e3bdaba16037900520022178fd8e1
SRIRAM ADARI <lakshmisriramadari1427@gmail.com>
2023-09-09 23:54:24 +0530

    Editor cursor update

--- Client/src/components/Editor.jsx
+++ Client/src/components/Editor.jsx
@@ -1,15 +1,17 @@
-import React, { useState,useEffect,useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import QuillCursors from 'quill-cursors'; // Import the quill-cursors library
 import { EditerSocket } from '../services/socketconnection';
-
+import Quill from 'quill';
+Quill.register('modules/cursors', QuillCursors); // Register the module
 
 const Editor = () => {
   const [fileContent, setFileContent] = useState(''); // Store text file content
   const [quillContent, setQuillContent] = useState(''); // Store Quill editor content
   const socket = useRef(null);
-
-
+  const quillRef = useRef(null); // Reference to the Quill editor instance
+  
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
     console.log(file); 
@@ -29,31 +31,58 @@ const Editor = () => {
 
   const handleContentChange = (content) => {
     setQuillContent(content);
-    socket.current.emit('editor-change', content); // Emit the change to other clients
+    socket.current.emit('editor-change', content);
   };
 
   useEffect(() => {
-    // Listen for changes from other users and update the Quill editor
     socket.current = EditerSocket().connect();
     socket.current.on('editor-change', (content) => {
       setQuillContent(content);
+      quillRef.current.setContents(content);
     });
 
     return () => {
-      socket.current.disconnect(); // Clean up the socket connection when the component unmounts
+      socket.current.disconnect();
     };
   }, [socket]);
+
+//   useEffect(() => {
+//     if (quillRef.current) {
+//         quillRef.current.setContents(content); // Enable cursor synchronization
+//     }
+//   }, [quillRef]);
+// useEffect(() => {
+//     socket.current = EditerSocket().connect();
+
+//     // Listen for changes from other users and update the Quill editor
+//     socket.current.on('editor-change', (content) => {
+//       if (quillRef.current) {
+//         // Check if quillRef.current is defined before using getModule
+//         quillRef.current.setContents(content); // Use setContents to update the Quill editor
+//       }
+//     });
+
+//     return () => {
+//       socket.current.disconnect();
+//     };
+//   }, [socket]);
+
   return (
     <div>
-      {/* <FileUpload onFileUpload={handleFileUpload} /> */}
       <input type="file" accept=".txt" onChange={handleFileUpload} />
       <ReactQuill
+        ref={quillRef}
         value={quillContent}
         onChange={handleContentChange}
         theme="snow"
+        modules={{
+          cursors: true, // Enable cursors module
+        }}
       />
     </div>
   );
 };
 
 export default Editor;
+
+
