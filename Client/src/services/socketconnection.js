ff410940a6582863f75609a2e33b0ba14b4dba9e
SRIRAM ADARI <lakshmisriramadari1427@gmail.com>
2023-09-10 06:46:06 +0530

    some shit

--- Client/src/services/socketconnection.js
+++ Client/src/services/socketconnection.js
@@ -1,25 +1,25 @@
 import { io } from 'socket.io-client';
 
-const initializeSocket = () => {
-  return io("http://localhost:8080/video"); 
-};
-
-export const ToChatSocket=()=>{
-  return io("http://localhost:8080/chat"); 
-}
-export const EditerSocket=()=>{
-  return io("http://localhost:8080/Editor"); 
-}
 // const initializeSocket = () => {
-//   return io("https://socialsync-eeuc.onrender.com/video"); 
+//   return io("http://localhost:8080/video"); 
 // };
 
 // export const ToChatSocket=()=>{
-//   return io("https://socialsync-eeuc.onrender.com/chat"); 
+//   return io("http://localhost:8080/chat"); 
 // }
 // export const EditerSocket=()=>{
-//   return io("https://socialsync-eeuc.onrender.com/Editor"); 
+//   return io("http://localhost:8080/Editor"); 
 // }
+const initializeSocket = () => {
+  return io("https://socialsync-eeuc.onrender.com/video"); 
+};
+
+export const ToChatSocket=()=>{
+  return io("https://socialsync-eeuc.onrender.com/chat"); 
+}
+export const EditerSocket=()=>{
+  return io("https://socialsync-eeuc.onrender.com/Editor"); 
+}
 
 
 export default initializeSocket;
